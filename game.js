b5a146be2c36ea5f813f4eef295158625fd2c230
thehyperwolf <52873243+thehyperwolf@users.noreply.github.com>
2023-03-02 12:39:04 +0530

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ let availableQuesions = [];
 let questions = [];
 
 fetch(
-    'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple'
+    'https://opentdb.com/api.php?amount=15&category=21&difficulty=easy&type=multiple'
 )
     .then((res) => {
         return res.json();
